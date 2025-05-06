f56a9f6c00f7289d50f4d07185697a1550deb81f
Sam Foreman <saforem2@gmail.com>
2025-05-06 13:24:43 -0500

    feat: Add `lua/configs/lspconfig.lua`

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "ruff", "pyright", "otter" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+
