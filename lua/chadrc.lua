64b57c92f2de56b9c63ce31e81f69e6033fab9fe
Sam Foreman <saforem2@gmail.com>
2025-05-06 13:24:50 -0500

    feat: Add `lua/chadrc.lua`

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,11 +14,15 @@ M.base46 = {
 	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = { load_on_startup = true }
+M.ui = {
+  statusline = {
+    theme = "minimal",
+    separator_style = "block"
+  }
+     --  tabufline = {
+     --     lazyload = false
+     -- }
+}
 
 return M
